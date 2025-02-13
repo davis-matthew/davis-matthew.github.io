55780654eed9f523d5241bcc5a2d81b5d2c393ae
davis-matthew <mdavis438@gatech.edu>
2025-02-13 02:41:22 -0500

    Update Feb 2025

--- pages/projects/projects.js
+++ pages/projects/projects.js
@@ -6,11 +6,25 @@
 //  Description
 const projects = [
     [
-        "THE-Spellchecker",
-        "../../resources/THE-Icon.png\" width = 150px height = 150px",
-        "https://danielwilliams.tech/spellchecker",
-        "Daniel Williams",
-        "Disappointed by the naïvety of existing spellcheckers for code, Daniel and I have set out on creating a tool which is better geared towards real-world code. We aim for convenient high precision spelling and grammatical fixes as well as other analyses ran on comments and strings within the code."
+        "THE-Software",
+        "../../resources/THE-Icon.png\"",
+        "https://github.com/davis-matthew/THE-Software",
+        "<a href=\"https://danielwilliams.tech/Resume.html\">Daniel Williams</a>",
+        "A software initiative designed to establish foundational software which is more capable, faster, and easier to use/extend. Some examples include: \
+        <ul>\
+            <li> <b>THE-Programming Language:</b> A C-inspired programming language updated with redesigns to address concerns and programmer desires. Prioritizes analysis amenability and overall efficiency</li>\
+            <li> <b>THE-Parser:</b> A fused lexer & parser which can handle any recursively enumerable grammar with error recovery and built-in optimization depending on parsing goal</li> \
+            <li> <b>THE-Compiler:</b> A front & middle-end for <b>THE-Programming Language</b>. Allows tuning of prioritization between speed, compilation time, memory usage, executable size, & energy usage</li> \
+            <li> <b>THE-Spellchecker:</b> A spelling & grammar checker built with code syntax in mind. High precision spelling and grammatical fixes as well as other analyses ran on comments and strings within the code</li> \
+        </ul>"
+    ],
+    [
+        "Frobenius",
+        "",
+        "",
+        "",
+        "A DSL & accompanying code generation tool that allows mathematicians to write a simple description of a combinatoric/brute-force math search and receive highly optimized code deployable as volunteer computing tasks. \
+        <b>Frobenius</b> automatically leverages relevant theorems and proven bounds to cut down the problem search space"
     ]
 ]
 
@@ -21,14 +35,16 @@ document.addEventListener("DOMContentLoaded", function(event){
 
 
 function createHTMLContent(project){
+    
     content = ""
     content += 
     "<div class = \"Project\">\
         <p class = \"Project-Title\">"+project[0]+"</p>\
         <div style=\"display:flex\">"
-    if(project[1] != ""){ 
+    if(project[1] != ""){
+        const [imgPath, imgStyle] = project[1].split(' ', 2)
         content += "\
-            <img class = \"Project-Image\" style=\"flex:1\" src=\""+project[1]+"\" alt=\""+project[0]+" Logo\"></img>"
+            <img class = \"Project-Image\" style=\"flex:1;"+imgStyle+"\" src=\""+imgPath+"\" alt=\""+project[0]+" Logo\"></img>"
     }
     content+="\
             <div style=\"flex:4\">\
--- pages/research/research.js
+++ pages/research/research.js
@@ -7,35 +7,51 @@
 //  Description
 const projects = [
     [
-        "Hybrid OpenMP Data Mapping Violation Detection & Repair (Working Title: OMPCompound)",
-        "../../resources/OMPCompound.png\" width = 200px height = 300px",
-        "2022 - Present",
-        "Dr. Vivek Sarkar, Georgia Institute of Technology",
+        "PoLLyMer - Invariant Generation via a Combination of LLMs, Static Transformations, and In-Context Verifier Feedback",
+        "../../resources/pollymer_flowchart.png",
+        "2024 - Present",
+        "<a href=\"https://vganesh1.github.io/\">Dr. Vijay Ganesh</a>",
+        "<a href=\"https://www.linkedin.com/in/ignacio-di-leva\">Ignacio Di Leva</a>",
+        "An LLM-based program invariant generation tool that consists of: <ul><li>Static analysis code transformations that pre-process the code</li><li>LLM that takes code as input and outputs a version with invariants</li><li>A feedback loop between the LLM and a verifier that allows the LLM to leverage in-context learning</li></ul>. This work is pending review at CAV '25."
+    ],
+    [
+        "AIR - Arbalest Instrumentation Reduction",
         "",
+        "2024 - Present",
+        "<a href=\"https://vsarkar.cc.gatech.edu/\">Dr. Vivek Sarkar</a>",
+        "<a href=\"http://lechenyu.io/\">Lechen Yu</a>",
+        "Arbalest is a dynamic analysis tool that detects OpenMP Data Mapping Violations. It instruments every single memory access on the host and kernel, but this is unnecessary. Static analysis can greatly reduce the amount of instrumented analysis calls."
+    ],
+    [
+        "Hybrid OpenMP Data Mapping Violation Detection & Repair (Working Title: OMPCompound)",
+        "../../resources/OMPCompound.png\" width:200px;height:300px",
+        "2022 - Present",
+        "<a href=\"https://vsarkar.cc.gatech.edu/\">Dr. Vivek Sarkar</a>",
+        "<a href=\"http://lechenyu.io/\">Lechen Yu</a>",
         "Existing tools OMPSanitizer (static), OmpMemOpt (static) & Arbalest (dynamic) perform analysis on OpenMP Data Mapping Violations. This project aims to combine the tools utilizing the best qualities of both tools and automatically repair the detected bugs."
     ],
     [
         "Extending OpenRace for GPU Race Detection",
         "",
         "2020 - 2021",
-        "Dr. Jeff Huang, Texas A&M University",
-        "Brad Swain, Coderrect Inc.",
+        "<a href=\"https://engineering.tamu.edu/cse/profiles/huang-jeff.html\">Dr. Jeff Huang</a>",
+        "<a href=\"https://www.linkedin.com/in/the-brad-swain\">Brad Swain</a>",
         "The formerly open-source OpenRace static analysis tool is great at finding race conditions in programs using OpenMP constructs, std::thread, & pthreads. However, not much had been done to account for races in OpenMP GPU Device constructs (target) or CUDA kernels. This project aimed to extend the OpenRace tool to be able to handle races within the offloaded kernels. The OpenRace tool was not geared towards accurate modeling of the GPU's many layers of structure (kernel calls, warps, threads, cooperative groups). Only a very basic but accurate starting model was implemented (CUDA 8 and before) which caught just a few constructed \"obvious\" races. This work was merged into the OpenRace tool prior to the tool becoming private."
     ],
     [
         "Dynamatic",
         "",
         "2019-2020",
-        "Dr. Jeff Huang, Texas A&M University",
-        "Dylan Theriot, Fatma Elsheimy",
-        "Dynamatic is a hybrid program analysis tool. This tool combined the analysis of Google's ThreadSanitizer and the HPCRace static analysis tool to find races in programs using OpenMP constructs. By leveraging overhead-reducing guided analysis from HPCRace, and Thread Sanitizer's extremely low false positive rate, we improved the performance on popular benchmark DataRaceBench, keeping all of the true positives, but changing all of the false positives of base OpenRace into true negatives without too much extra computation overhead. This work is published here: <a href = \"https://hdl.handle.net/1969.1/194411\">LINK</a>"
+        "<a href=\"https://engineering.tamu.edu/cse/profiles/huang-jeff.html\">Dr. Jeff Huang</a>",
+        "<a href=\"https://dylantheriot.com/#/\">Dylan Theriot</a>, Fatma Elsheimy",
+        "Dynamatic is a hybrid program analysis tool. This tool combined the analysis of Google's ThreadSanitizer and the HPCRace static analysis tool to find races in programs using OpenMP constructs. By leveraging overhead-reducing guided analysis from HPCRace, and Thread Sanitizer's extremely low false positive rate, we improved the performance on popular benchmark DataRaceBench, keeping all of the true positives, but changing all of the false positives of base OpenRace into true negatives without too much extra computation overhead. This work is published as an undergraduate thesis here: <a href = \"https://hdl.handle.net/1969.1/194411\">LINK</a>"
     ],
     [
         "NEO-UFO",
         "",
         "2019 - 2019",
-        "Dr. Jeff Huang, Texas A&M University",
-        "Yahui Sun, Matthew Chen, Andrew Chin, Andreas Tsouloupas",
+        "<a href=\"https://engineering.tamu.edu/cse/profiles/huang-jeff.html\">Dr. Jeff Huang</a>",
+        "<a href=\"https://www.linkedin.com/in/yahui-sun\">Yahui Sun</a>, Matthew Chen, Andrew Chin, Andreas Tsouloupas",
         "<a href=\"https://dl.acm.org/doi/10.1145/3180155.3180225\">UFO</a> is a dynamic program analysis tool. This tool finds Use-After-Free (UAF) bugs while chromium is running through the use of trace analysis with Google ThreadSanitizer. The goal is to have this tool run in the background and create a crash log/report to send the engineers whenever a user would encounter one of these bugs. We extended UFO to perform predictive analysis using the <a href=\"http://umathur3.web.engr.illinois.edu/papers/wcp-pldi2017.pdf\">WCP algorithm</a>. I wrote a <a href = \"https://github.com/davis-matthew/NEO-SVF\">Static Analysis pass</a> to read through the LLVM IR of chromium and identify regions which were unlikely to have a UAF. This information was then turned into a Thread Sanitizer blacklist file to toggle on/off expensive analysis, reducing the overhead on profiling tests from ~11x average to ~6-7x."
     ]
 ]
@@ -53,8 +69,9 @@ function createHTMLContent(research){
         <p class = \"Project-Title\">"+research[0]+"</p>\
         <div style=\"display:flex\">"
     if(research[1] != ""){ 
+        const [imgPath, imgStyle] = research[1].split(' ', 2)
         content += "\
-            <img class = \"Project-Image\" style=\"flex:1\" src=\""+research[1]+"\" alt=\""+research[0]+" Logo\"></img>"
+            <img class = \"Project-Image\" style=\"flex:1;"+imgStyle+"\" src=\""+imgPath+"\" alt=\""+research[0]+" Logo\"></img>"
     }
     content+="\
             <div style=\"flex:4\">\
